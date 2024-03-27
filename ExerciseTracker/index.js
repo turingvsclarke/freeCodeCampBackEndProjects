9e0dc48a9b3842cd122a052d5f9c2096e5cf239d
turingvsclarke Eades <rhinoeades@gmail.com>
2024-03-27 20:06:14 +0000

    feat: post/get requests to api/users

--- ExerciseTracker/index.js
+++ ExerciseTracker/index.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser=require('body-parser');
 require('dotenv').config()
+
+let id=1;
+let users=[];
 
 app.use(cors())
 app.use(express.static('public'))
+// Trying this out. Saw it in a rap video once
+app.use(bodyParser({extended:false}));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users',(req,res)=>{
+  // Create new user
+  let newUser={username:req.body.username,_id:id++}
+  users.push(newUser);
+  res.json(newUser);
+})
+
+app.get('/api/users',(req,res)=>{
+  res.send(users);
+})
 
 
 
